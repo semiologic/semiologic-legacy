8ae78b611ab9c8043de8e7e11807a0262c619fa1
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-23 21:26:38 +0000

    updater fix -- use site name rather than site url as default copyright
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/semiologic/trunk@639 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/autoinstall.php
+++ inc/autoinstall.php
@@ -5,9 +5,7 @@
 # Set default captions
 #
 
-$site_url = get_option('home');
-$site_url = preg_replace("|^.+://|", '', $site_url);
-$site_url = preg_replace("|/.*$|", '', $site_url);
+$site_name = get_option('blogname');
 
 $sem_captions = array(
 	"1_comment_link" => "1 Comment",
@@ -16,7 +14,7 @@ $sem_captions = array(
 	"comments_on" => "Comments on %title%",
 	"comment_permalink" => "Permalink",
 	"comment_trackback" => "Trackback URI",	
-	"copyright" => "Copyright %year%, $site_url",
+	"copyright" => "Copyright %year%, $site_name",
 	"email_field" => "Email",
 	"email_link" => "Email",
 	"filed_under" => "Filed under %categories% by %author%",
