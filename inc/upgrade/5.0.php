9db7964cba0ab0a6130e67180831601e7049dbe0
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-24 15:22:50 +0000

    fix 5.0 nav menu importer
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/semiologic/trunk@650 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/upgrade/5.0.php
+++ inc/upgrade/5.0.php
@@ -118,6 +118,17 @@ foreach ( $sem_nav as $nav_menu => $nav_items )
 			
 			continue;
 		}
+		
+		if ( strpos($ref, '@') !== false )
+		{
+			$items[] = array(
+				'type' => 'url',
+				'label' => $label,
+				'ref' => 'mailto:' . $ref,
+				);
+			
+			continue;
+		}
 
 		if ( !$ref )
 		{
