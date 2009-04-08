35b1e95ca7ef7f8b346fe57fcb2a8d1ec096fe5d
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-04-08 06:52:39 +0000

    bump theme version numbers
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/semiologic/trunk@1058 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/init.php
+++ inc/init.php
@@ -3,7 +3,7 @@
 # sem_version
 #
 
-define('sem_version', '5.7.1');
+define('sem_version', '5.7.2');
 
 define('sem_path', dirname(dirname(__FILE__)));
 define('sem_url', get_stylesheet_directory_uri());
