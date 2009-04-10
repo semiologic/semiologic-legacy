f2dc7da75de5ea163c0e1c8b64b4c9ba427ccf64
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-04-10 09:35:20 +0000

    fix link widget upgrader
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/semiologic/trunk@1147 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/upgrade/options.php
+++ inc/upgrade/options.php
@@ -82,11 +82,36 @@ function sem_clean_up_options()
 	delete_option('semiologic');
 	
 	if ( !class_exists('link_widgets') && $sem_opt = get_option('link_widgets') ) {
-		$wp_opt = get_option('widget_links');
-		if ( !isset($wp_opt['_multiwidget']) ) {
-			$sem_opt['_multiwidget'] = 1;
-			update_option('widget_links' ,$sem_opt);
+		global $wpdb;
+
+		$sem_opt['_multiwidget'] = 1;
+		foreach ( $sem_opt as $k => $opt ) {
+			if ( !is_numeric($k) )
+				continue;
+			if ( isset($opt['filter']) )
+				$sem_opt[$k]['category'] = $opt['filter'];
 		}
+		update_option('widget_links', $sem_opt);
+
+		$sidebars_widgets = get_option('sidebars_widgets');
+		foreach ( $sidebars_widgets as $sidebar => $widgets ) {
+			if ( !is_array($widgets) ) continue;
+			foreach ( $widgets as $k => $widget ) {
+				if ( strpos($widget, 'link_widget') === 0 ) {
+					$new_widget = str_replace("link_widget", "links", $widget);
+					$sidebars_widgets[$sidebar][$k] = $new_widget;
+					if ( $sidebar == 'inline_widgets' ) {
+						$wpdb->query("
+							UPDATE	$wpdb->posts
+							SET		post_content = replace(post_content, '[widget:" . $wpdb->escape($widget) . "]', '[widget:" . $wpdb->escape($new_widget) . "]')
+							WHERE	post_content LIKE '%[widget:" . $wpdb->escape($widget) . "]%'
+							");
+					}
+				}
+			}
+		}
+		update_option('sidebars_widgets', $sidebars_widgets);
+
 		delete_option('link_widgets');
 	}
 	
