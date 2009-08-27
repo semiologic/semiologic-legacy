53b27fdb488a8bb05aa6495b5bdc1a4a2d1ff514
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-08-27 11:32:34 +0000

    avoid carnage on old sites
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/semiologic/trunk@2451 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/widgets.php
+++ inc/widgets.php
@@ -2948,7 +2948,7 @@ class footer extends sem_nav_menu {
 		$year = date('Y');
 		$site_name = strip_tags(get_option('blogname'));
 		
-		$copyright = sprintf($copyright, $year, $site_name);
+		$copyright = sprintf($copyright, $site_name, $year);
 		
 		if ( $copyright ) {
 			echo '<div id="copyright_notice">';
