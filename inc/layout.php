bb271391016549577e1249c8bf8814362d74c270
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-10-14 18:56:08 +0000

    fix layout switcher issue
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/semiologic/trunk@3113 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/layout.php
+++ inc/layout.php
@@ -69,7 +69,7 @@ class sem_layout {
 
 <script type="text/javascript">
 jQuery(document).ready(function() {
-	jQuery("#available_options label").click(function() {
+	jQuery(".available_options label").click(function() {
 		jQuery(this).closest('td').find('input:radio').attr('checked', 'checked');
 		jQuery('#option_picker').trigger('submit');
 	});
