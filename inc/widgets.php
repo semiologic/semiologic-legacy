0c3c034ef0fd34dbce8431665347d8703bf69194
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-11-20 15:37:04 +0000

    more local url handling
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/themes/semiologic/trunk@3380 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- inc/widgets.php
+++ inc/widgets.php
@@ -2477,9 +2477,10 @@ class sem_nav_menu extends WP_Widget {
 	 * @return bool $is_local_url
 	 **/
 
-	function is_local_url($url) {
 		if ( in_array(substr($url, 0, 1), array('?', '#')) || strpos($url, '://') === false )
 			return true;
+		elseif ( preg_match("~/go(/|\.)~i", $url) )
+			return false;
 		
 		static $site_domain;
 		
